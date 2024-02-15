f264278ebd29c899482ed3f8e9c967ec69999351
mhtruong1031 <mhtruong1031@gmail.com>
2024-02-15 13:03:06 -0800

    push

--- dataset_classification.py
+++ dataset_classification.py
@@ -1,16 +1,20 @@
 import pandas as pd
 from shutil import copy
 
-PH2 = pd.read_excel('resources/PH2/PH2Dataset/PH2_dataset.xlsx')
+def main() -> None:
+    PH2 = pd.read_excel('resources/PH2/PH2Dataset/PH2_dataset.xlsx')
 
-com = PH2[PH2["Unnamed: 2"] == 'X']["Unnamed: 0"]
-aty = PH2[PH2["Unnamed: 3"] == 'X']["Unnamed: 0"]
-mel = PH2[PH2["Unnamed: 4"] == 'X']["Unnamed: 0"]
+    com = PH2[PH2["Unnamed: 2"] == 'X']["Unnamed: 0"]
+    aty = PH2[PH2["Unnamed: 3"] == 'X']["Unnamed: 0"]
+    mel = PH2[PH2["Unnamed: 4"] == 'X']["Unnamed: 0"]
 
-classes = [(com, 'com'), (aty, 'aty'), (mel, 'mel')]
+    classes = [(com, 'com'), (aty, 'aty'), (mel, 'mel')]
 
-for id_list, type in classes:
-    for id in id_list:
-        target      = f'resources/PH2/PH2Dataset/PH2_Dataset_images/{id}/{id}_Dermoscopic_Image/{id}.bmp'
-        destination = f'resources/PH2/Classified_Dataset/{type}/{id}.bmp'
-        copy(target, destination)
+    for id_list, type in classes:
+        for id in id_list:
+            target      = f'resources/PH2/PH2Dataset/PH2_Dataset_images/{id}/{id}_Dermoscopic_Image/{id}.bmp'
+            destination = f'resources/PH2/Classified_Dataset/{type}/{id}.bmp'
+            copy(target, destination)
+
+if __name__ == '__main__':
+    main()
