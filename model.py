e4a57bfe0b328c260517d4db04e0b6b04942c0d2
mhtruong1031 <mhtruong1031@gmail.com>
2024-02-15 17:53:14 -0800

    model training

--- model.py
+++ model.py
@@ -3,9 +3,9 @@ import tensorflow as tf
 from keras import layers
 
 input_shape = (765,572,3)
-kernal_size = (5, 5)
+kernel_size = (5, 5)
 
-# TODO: Import Dataset
+# Import Dataset
 training_ds = keras.utils.image_dataset_from_directory(
     directory  = 'resources/PH2/training_data',
     labels     = 'inferred',
@@ -21,27 +21,61 @@ testing_ds = keras.utils.image_dataset_from_directory(
 
 # VGG-16 Instantiation
 model = tf.keras.Sequential([
-    layers.Conv2D(input_shape=input_shape, kernal_size=kernal_size, padding='same', activation='relu'),
-    layers.Conv2D(filters=64, kernal_size=kernal_size, padding='same', activation='relu'),
+    layers.Conv2D(input_shape=input_shape, filters=64, kernel_size=kernel_size, padding='same', activation='relu'),
+    layers.Conv2D(filters=64, kernel_size=kernel_size, padding='same', activation='relu'),
     layers.MaxPool2D(pool_size=(2, 2), strides=(2,2)),
-    layers.Conv2D(filters=128, kernal_size=kernal_size, padding='same', activation='relu'),
-    layers.Conv2D(filters=128, kernal_size=kernal_size, padding='same', activation='relu'),
+    layers.Conv2D(filters=128, kernel_size=kernel_size, padding='same', activation='relu'),
+    layers.Conv2D(filters=128, kernel_size=kernel_size, padding='same', activation='relu'),
     layers.MaxPool2D(pool_size=(2, 2), strides=(2,2)),
-    layers.Conv2D(filters=256, kernal_size=kernal_size, padding='same', activation='relu'),
-    layers.Conv2D(filters=256, kernal_size=kernal_size, padding='same', activation='relu'),
-    layers.Conv2D(filters=256, kernal_size=kernal_size, padding='same', activation='relu'),
+    layers.Conv2D(filters=256, kernel_size=kernel_size, padding='same', activation='relu'),
+    layers.Conv2D(filters=256, kernel_size=kernel_size, padding='same', activation='relu'),
+    layers.Conv2D(filters=256, kernel_size=kernel_size, padding='same', activation='relu'),
     layers.MaxPool2D(pool_size=(2, 2), strides=(2,2)),
-    layers.Conv2D(filters=512, kernal_size=kernal_size, padding='same', activation='relu'),
-    layers.Conv2D(filters=512, kernal_size=kernal_size, padding='same', activation='relu'),
-    layers.Conv2D(filters=512, kernal_size=kernal_size, padding='same', activation='relu'),
+    layers.Conv2D(filters=512, kernel_size=kernel_size, padding='same', activation='relu'),
+    layers.Conv2D(filters=512, kernel_size=kernel_size, padding='same', activation='relu'),
+    layers.Conv2D(filters=512, kernel_size=kernel_size, padding='same', activation='relu'),
     layers.MaxPool2D(pool_size=(2, 2), strides=(2,2)),
-    layers.Conv2D(filters=512, kernal_size=kernal_size, padding='same', activation='relu'),
-    layers.Conv2D(filters=512, kernal_size=kernal_size, padding='same', activation='relu'),
-    layers.Conv2D(filters=512, kernal_size=kernal_size, padding='same', activation='relu'),
+    layers.Conv2D(filters=512, kernel_size=kernel_size, padding='same', activation='relu'),
+    layers.Conv2D(filters=512, kernel_size=kernel_size, padding='same', activation='relu'),
+    layers.Conv2D(filters=512, kernel_size=kernel_size, padding='same', activation='relu'),
     layers.MaxPool2D(pool_size=(2, 2), strides=(2,2)),
     layers.Flatten(),
     layers.Dense(units=4096, activation='relu'),
     layers.Dense(units=4096, activation='relu'),
-    layers.Dense(units=3, activation='relu')
+    layers.Dense(units=3, activation='softmax')
 ])
+
+# Model Compilation
+model.compile(
+    optimizer = keras.optimizers.Adam(lr=0.001), # learning rate
+    loss      = keras.losses.categorical_crossentropy,
+    metrics   = ['accuracy']
+)
+
+checkpoint = keras.callbacks.ModelCheckpoint( # monitor accuracy
+    'vgg16_1.h5',
+    monitor           = 'val_acc', 
+    verbose           = 1, # type of progres display
+    save_best_only    = True,
+    save_weights_only = False,
+    mode              = 'auto',
+    period            = 1
+)
+
+early = keras.callbacks.EarlyStopping( # early stopping if there is no improvement
+    monitor   = 'val_acc',
+    min_delta = 0,
+    patience  = 20, # early stop if no improvement in 20 epochs
+    verbose   = 1,
+    mode      = 'auto'
+)
+
+hist = model.fit_generator(
+    steps_per_epoch = 100,
+    generator = training_ds,
+    validation_data = testing_ds,
+    validation_steps = 10,
+    epochs = 100,
+    callbacks = [checkpoint, early]
+)
 
